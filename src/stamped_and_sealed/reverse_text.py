57bd2061d9d15bce8deec21c0da8292606305fd6
ishy04 <152747257+ishy04@users.noreply.github.com>
2025-03-20 13:05:19 -0400

    fixed reverse_text
    contribution made by ethan yu

--- src/stamped_and_sealed/reverse_text.py
+++ src/stamped_and_sealed/reverse_text.py
@@ -1,8 +1,14 @@
-def reverse_text(text: str) -> str:
+def reverse_text(text: str, ignore_spaces: bool = False, ignore_punctuation: bool = False) -> str:
     """
     Reverses the given text.
 
     :param text: The input string to reverse.
+    :param ignore_spaces: If True, spaces are removed before reversing.
+    :param ignore_punctuation: If True, punctuation is removed before reversing.
     :return: The reversed string.
     """
-    return text[::-1] 
+    if ignore_spaces:
+        text = text.replace(" ", "")
+    if ignore_punctuation:
+        text = ''.join(char for char in text if char.isalnum() or char.isspace())
+    return text[::-1]
